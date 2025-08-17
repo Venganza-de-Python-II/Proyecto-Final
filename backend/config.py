cc317234495c673107df764cd487f63537bc6f8e
Fl2on <69091361+fl2on@users.noreply.github.com>
2025-08-16 20:22:37 -0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    # Variables de entorno con valores por defecto adecuados
+    # Variables de entorno
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:admin123@localhost:27017/?authSource=admin")
     MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "talleresdb")
     JWT_SECRET = os.getenv("JWT_SECRET", "supersecreto")
